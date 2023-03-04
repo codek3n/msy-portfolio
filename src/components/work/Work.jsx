7793717ec7055c8805b475f145c57b34e2942f02
codek3n <msycodebiz@gmail.com>
2023-03-05 04:05:03 +0800

    add projects

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
-import caster1 from '../../assets/images/caster1.jpg';
-import caster2 from '../../assets/images/caster2.jpg';
-import { RxTriangleRight } from 'react-icons/rx';
-// import { BsBoxArrowUpRight } from 'react-icons/bs';
-// import { HiArrowTopRightOnSquare } from 'react-icons/hi2';
-import { HiArrowTopRightOnSquare } from 'react-icons/hi2';
-import { FiGithub } from 'react-icons/fi';
 
 import './work.scss';
 
+import Project1 from './project-1/Project1';
+import Project2 from './project-1/Project2';
+
 
 const Work = () => {
-  const [isHovering, setIsHovering] = useState(false)
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <div className='work' id='projects'>
       <div className='work__container'>
@@ -32,71 +29,17 @@ const Work = () => {
 
       <div className='work__projects__container'>
 
-        <div className='work__project project--1'>
+        <Project1 isHovering={isHovering} setIsHovering={setIsHovering} />
 
-          <div className='project__card'>
+        <br />
+        <br />
 
-            <p className='project__featured--project'>
-              Featured Project
-            </p>
-
-            <div className='project__header__container'>
-
-              <h2 className='project__title'>
-                Caster
-              </h2>
-
-              <div className='header__icon__container'>
-
-                <a href="https://github.com/codek3n/caster-movie-trailer-app-2" target='_blank'>
-                  <FiGithub className='header__icon' />
-                </a>
-
-                <a href="https://caster-movie-app.netlify.app/" target='_blank'>
-                  <HiArrowTopRightOnSquare className='header__icon arrow__redirect' />
-                </a>
-
-              </div>
-
-            </div>
-
-            <br />
-
-            <div className='project__image__container' onMouseMove={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
-
-              {/* {isHovering ?
-                <img src={caster2} alt='movie app picture' className='project__image' />
-                : <img src={caster1} alt='movie app picture' className='project__image' />} */}
-
-              {isHovering ?
-                <a href="https://caster-movie-app.netlify.app/" target='_blank'>
-                  <img src={caster2} alt='movie app picture' className='project__image' />
-                </a>
-                : <a href="https://caster-movie-app.netlify.app/" target='_blank'>
-                  <img src={caster1} alt='movie app picture' className='project__image' />
-                </a>}
-
-            </div>
-
-            <div className='project__description__container'>
-              <div>
-                <RxTriangleRight className='bullet--icon' />
-              </div>
-              <p className='project__description'>
-                Caster is a <span>movie app</span>  that is perfect place to discover new movies and watch their trailers.
-                With a simple and easy-to-use interface,
-                you can quickly browse through its <span>extensive collection of trailers</span>  and find the perfect movie to watch.
-                You can search for specific movies, filter by genre or release date.
-                Stay up-to-date with the latest movie news and never miss a trailer release again.
-              </p>
-            </div>
-
-
-          </div>
-
-        </div>
+        <Project2 isHovering={isHovering} setIsHovering={setIsHovering} />
 
       </div>
+
+
+
     </div>
   )
 }
---
+++ src/components/work/project-1/Project2.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+import zcShoppy1 from '../../../assets/images/zc-shoppy1.jpg';
+import zcShoppy2 from '../../../assets/images/zc-shoppy2.jpg';
+import { RxTriangleRight } from 'react-icons/rx';
+import { HiArrowTopRightOnSquare } from 'react-icons/hi2';
+import { FiGithub } from 'react-icons/fi';
+
+const Project2 = () => {
+  const [isHovering, setIsHovering] = useState(false)
+
+  return (
+    <div className='work__project project--2'>
+
+      <div className='project__card card--2'>
+
+        <p className='project__featured--project'>
+          Featured Project
+        </p>
+
+        <div className='project__header__container'>
+
+          <h2 className='project__title'>
+            ZC-Shoppy
+          </h2>
+
+          <div className='header__icon__container'>
+
+            <a href="https://github.com/codek3n/zc-shoppy" target='_blank'>
+              <FiGithub className='header__icon' />
+            </a>
+
+            <a href="https://zc-shoppy.netlify.app/" target='_blank'>
+              <HiArrowTopRightOnSquare className='header__icon arrow__redirect' />
+            </a>
+
+          </div>
+
+        </div>
+
+        <br />
+
+        <div className='project__container project__container--2'>
+
+          <div className='project__image__container project__image--2' onMouseMove={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+
+            {isHovering ?
+              <a href="https://zc-shoppy.netlify.app/" target='_blank'>
+                <img src={zcShoppy2} alt='movie app picture' className='project__image' />
+              </a>
+              : <a href="https://zc-shoppy.netlify.app/" target='_blank'>
+                <img src={zcShoppy1} alt='movie app picture' className='project__image' />
+              </a>}
+
+          </div>
+
+          <div className='project__description__container'>
+            <div>
+              <RxTriangleRight className='bullet--icon' />
+            </div>
+            <p className='project__description'>
+              My first frontend project was a responsive <span>online shopping web app</span>  for a small business.
+              Although the app didn't have payment integration, it was designed to showcase the business's products
+              and to provide customers with an easy way to browse and contact the business.
+              Through this project, I developed my skills in frontend development,
+              including designing a user-friendly and visually appealing app that meets the needs of both the business and its customers.
+            </p>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Project2
